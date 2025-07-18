d4f020e61a7659f211506e9c43d7274be49c72fe
elijahhkai <ohbrandner@gmail.com>
2025-07-18 02:28:56 -1000

    Contats table finished in 73 minutes

--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -4,15 +4,18 @@ import { Contact } from '@prisma/client';
 import { Card, Image } from 'react-bootstrap';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
-const ContactCard = ({ contact }: { contact: Contact }) => (
+const ContactCardAdmin = ({ contact }: { contact: Contact }) => (
   <Card className="h-100">
     <Card.Header>
       <Image src={contact.image} width={75} />
       <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
       <Card.Subtitle>{contact.address}</Card.Subtitle>
     </Card.Header>
-    <Card.Body>{contact.description}</Card.Body>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+      <p className="blockquote-footer">{contact.owner}</p>
+    </Card.Body>
   </Card>
 );
 
-export default ContactCard;
+export default ContactCardAdmin;
